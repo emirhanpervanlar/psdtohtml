b8817244b95684af7a7931446cef98c05e5d5add
Emirhan Pervanlar <pervanlaremirhan@gmail.com>
2017-04-17 01:57:38 +0300

    Daha önceden hazırladığımız dosyalarımızı aktardık

--- index.php
+++ index.php
@@ -6,11 +6,12 @@
 <link href="css/bootstrap.min.css" rel="stylesheet">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/material-design-iconic-font/2.2.0/css/material-design-iconic-font.min.css">
 <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Coda:400,800" rel="stylesheet">
 </head>
 
 <div id="topbar">
 <div class="container">	
-	<div class="topbarlogo"></div>
+	<div class="topbarlogo"><H1>LOGO</H1></div>
 		<div class="topbarmenu">
 			
 			<div class="topbarsearch">
@@ -34,17 +35,21 @@
 		<div class="container">
 			<div class="menu-content">
 			<li>ENTAIREMENT</li>
+			<li>LİFESTYLE</li>
+			<li>TEKNOLOGY</li>
 			<li>ENTAIREMENT</li>
-			<li>ENTAIREMENT</li>
-			<li>ENTAIREMENT</li>
-			<li>ENTAIREMENT</li>
+			<li>LIFESTYLE</li>
+			<li>SPORTS</li>
 			</div>
 		</div>
 		</div>
 
 <div class="topbarbottom">
 <div class="container">	
-	
+	<div class="topbarbottomlatest"><p>August .12 .2012 - Syrian prime minister defects, fighting goes on</p></div>
+    <div class="shoppingcart"><p>Shopping Cart: <span>0</span> item(s) - $0.00</p>
+    <div class="shoppingcartarrow"><i class="zmdi zmdi-caret-down"></i></div>
+    </div>
 </div>
 </div>
 </div>
@@ -55,10 +60,13 @@
 			<i class="zmdi zmdi-chevron-left"></i>
 		</div>
 	</div>
-	<div class="sliderarticle">		
+	<div class="sliderarticle">
+        <div class="sliderimg"><img src="img/slider1.png"/></div>
 		<div class="slidercontent">
-			<h2>3012 Mint Yeşili Büyülü Abiye</h2>
-		</div>
+			<h2>Where can I get some?</h2>
+            <p>Fusce ultrices ornare velit, consectetur tempus eros dapibus et. Integer lobortis, dui in iaculis sollicitudin, felis nunc aliquam nibh, eu porta nisi urna nec odio. Duis suscipit viverra magna id sagittis. Quisque odio neque, condimentum cursus volutpat vel, pharetra ac nibh. Cras cursus libero id nunc facilisis luctus. Aenean ultricies, risus cursus sollicitudin congue, diam diam congue velit, ut sodales sem enim a nisl. Aenean elit diam, mollis fermentum pellentesque nec, convallis eu est. Nunc eu mattis dui. Nulla quis suscipit ligula. Morbi diam massa, laoreet id pretium id, varius et sem.</p>
+            <a href="#">Shop Now</a>
+        </div>
 	</div>
 	<div class="sliderright">
 		<div class="sliderrightbutton">
@@ -69,13 +77,224 @@
 <div class="container">
 <div class="homesidebar">
 	<div class="homesidebarfeatured">
-		<div class="featuredtitle">FEATURED</div><div class=""></div>
-		
+
+		<div class="featuredtitle">FEATURED</div>
+        <div class="sidebarfeaturedi"><i class="zmdi zmdi-chevron-left"></i><i class="zmdi zmdi-chevron-right"></i></div>
+        <div class="sidebarfeaturedarticle">
+            <div class="sidebarfeaturedarticlebanner"><p>New</p> </div>
+            <div class="sidebarfeaturedarticleimg"><img src="img/featureditem1img.png"/></div>
+            <h2>Lorem ipsum dolor sit</h2>
+            <div class="sidebarfeaturedarticlebuy"><i class="zmdi zmdi-shopping-cart-plus"></i></div>
+            <p class="sidebarfeaturedarticlepred">$34.99</p><p class="sidebarfeaturedarticlepgray">$69.99</p>
+
+        </div>
 	</div>
+    <div class="trendingnow">
+        <div class="trendingnowtitle"><h2>TRENDING NOW</h2></div>
+        <div class="trendingnowarticle">
+
+                <li>Man</li>
+                <li>Women</li>
+                <li>Kids</li>
+                <li>Accessories</li>
+        </div>
+    </div>
 </div>
-<div class="homearticle">
-	<div class="homearticletitle"><h2>YENİ ÜRÜNLER</h2></div>
-</div>
+    <div class="homearticle">
+        <div class="newproducttitle"><h2>NEW PRODUCTS</h2></div>
+        <div class="newproductarticle">
+            <div class="newproductarticlelist">
+                <div class="newproductarticlebox">
+
+                    <div class="newproductarticleboximg">
+                        <div class="productbanner"><p>Sale</p> </div>
+                        <img src="img/productitem1img.png"/></div>
+                    <h2>Lorem ipsum dolor sit</h2>
+                    <div class="newproductarticleboxbuy"><i class="zmdi zmdi-shopping-cart-plus"></i></div>
+                    <p class="newproductarticleboxpgray">$69.99</p><p class="newproductarticleboxpred">$34.99</p>
+                </div>
+                <div class="newproductarticlebox">
+                    <div class="newproductarticleboximg">
+                        <div class="productnotbanner"></div>
+                        <img src="img/productitem2img.png"/></div>
+                    <h2>Lorem ipsum dolor sit</h2>
+                    <div class="newproductarticleboxbuy"><i class="zmdi zmdi-shopping-cart-plus"></i></div>
+                    <p class="newproductarticleboxpred">$34.99</p>
+                </div>
+                <div class="newproductarticlebox">
+
+                    <div class="newproductarticleboximg">
+                        <div class="productnotbanner"></div>
+                        <img src="img/productitem3img.png"/></div>
+                    <h2>Lorem ipsum dolor sit</h2>
+                    <div class="newproductarticleboxbuy"><i class="zmdi zmdi-shopping-cart-plus"></i></div>
+                    <p class="newproductarticleboxpred">$34.99</p>
+                </div>
+                <div class="newproductarticlebox">
+                    <div class="newproductarticleboximg">
+                        <div class="productnotbanner"></div>
+                        <img src="img/productitem1img.png"/></div>
+                    <h2>Lorem ipsum dolor sit</h2>
+                    <div class="newproductarticleboxbuy"><i class="zmdi zmdi-shopping-cart-plus"></i></div>
+                    <p class="newproductarticleboxpgray">$69.99</p><p class="newproductarticleboxpred">$34.99</p>
+                </div>
+                <div class="newproductarticlebox">
+                    <div class="newproductarticleboximg">
+                        <div class="productbanner"><p>Sale</p> </div>
+                        <img src="img/productitem2img.png"/></div>
+                    <h2>Lorem ipsum dolor sit</h2>
+                    <div class="newproductarticleboxbuy"><i class="zmdi zmdi-shopping-cart-plus"></i></div>
+                    <p class="newproductarticleboxpred">$34.99</p>
+                </div>
+                <div class="newproductarticlebox">
+                    <div class="newproductarticleboximg">
+                        <div class="productbanner"><p>Sale</p> </div>
+                        <img src="img/productitem3img.png"/></div>
+                    <h2>Lorem ipsum dolor sit</h2>
+                    <div class="newproductarticleboxbuy"><i class="zmdi zmdi-shopping-cart-plus"></i></div>
+                    <p class="newproductarticleboxpred">$34.99</p>
+                </div>
+                <div class="newproductarticlebox">
+                    <div class="newproductarticleboximg">
+                        <div class="productnotbanner"></div>
+                        <img src="img/productitem1img.png"/></div>
+                    <h2>Lorem ipsum dolor sit</h2>
+                    <div class="newproductarticleboxbuy"><i class="zmdi zmdi-shopping-cart-plus"></i></div>
+                    <p class="newproductarticleboxpgray">$69.99</p><p class="newproductarticleboxpred">$34.99</p>
+                </div>
+                <div class="newproductarticlebox">
+                    <div class="newproductarticleboximg">
+                        <div class="productnotbanner"></div>
+                        <img src="img/productitem2img.png"/></div>
+                    <h2>Lorem ipsum dolor sit</h2>
+                    <div class="newproductarticleboxbuy"><i class="zmdi zmdi-shopping-cart-plus"></i></div>
+                    <p class="newproductarticleboxpred">$34.99</p>
+                </div>
+                <div class="newproductarticlebox">
+                    <div class="newproductarticleboximg">
+                        <div class="productnotbanner"></div>
+                        <img src="img/productitem3img.png"/></div>
+                    <h2>Lorem ipsum dolor sit</h2>
+                    <div class="newproductarticleboxbuy"><i class="zmdi zmdi-shopping-cart-plus"></i></div>
+                    <p class="newproductarticleboxpred">$34.99</p>
+                </div>
+                <div class="newproductarticlebox">
+
+                    <div class="newproductarticleboximg">
+                        <div class="productnotbanner"></div>
+                        <img src="img/productitem1img.png"/></div>
+                    <h2>Lorem ipsum dolor sit</h2>
+                    <div class="newproductarticleboxbuy"><i class="zmdi zmdi-shopping-cart-plus"></i></div>
+                    <p class="newproductarticleboxpgray">$69.99</p><p class="newproductarticleboxpred">$34.99</p>
+                </div>
+                <div class="newproductarticlebox">
+                    <div class="newproductarticleboximg">
+                        <div class="productnotbanner"></div>
+                        <img src="img/productitem2img.png"/></div>
+                    <h2>Lorem ipsum dolor sit</h2>
+                    <div class="newproductarticleboxbuy"><i class="zmdi zmdi-shopping-cart-plus"></i></div>
+                    <p class="newproductarticleboxpred">$34.99</p>
+                </div>
+                <div class="newproductarticlebox">
+                    <div class="newproductarticleboximg">
+                        <div class="productnotbanner"></div>
+                        <img src="img/productitem3img.png"/></div>
+                    <h2>Lorem ipsum dolor sit</h2>
+                    <div class="newproductarticleboxbuy"><i class="zmdi zmdi-shopping-cart-plus"></i></div>
+                    <p class="newproductarticleboxpred">$34.99</p>
+                </div>
+                <div class="newproductarticlebox">
+                    <div class="newproductarticleboximg">
+                        <div class="productnotbanner"></div>
+                        <img src="img/productitem1img.png"/></div>
+                    <h2>Lorem ipsum dolor sit</h2>
+                    <div class="newproductarticleboxbuy"><i class="zmdi zmdi-shopping-cart-plus"></i></div>
+                    <p class="newproductarticleboxpgray">$69.99</p><p class="newproductarticleboxpred">$34.99</p>
+                </div>
+                <div class="newproductarticlebox">
+                    <div class="newproductarticleboximg">
+                        <div class="productnotbanner"></div>
+                        <img src="img/productitem2img.png"/></div>
+                    <h2>Lorem ipsum dolor sit</h2>
+                    <div class="newproductarticleboxbuy"><i class="zmdi zmdi-shopping-cart-plus"></i></div>
+                    <p class="newproductarticleboxpred">$34.99</p>
+                </div>
+                <div class="newproductarticlebox">
+                    <div class="newproductarticleboximg">
+                        <div class="productnotbanner"></div>
+                        <img src="img/productitem3img.png"/></div>
+                    <h2>Lorem ipsum dolor sit</h2>
+                    <div class="newproductarticleboxbuy"><i class="zmdi zmdi-shopping-cart-plus"></i></div>
+                    <p class="newproductarticleboxpred">$34.99</p>
+                </div>
+
+            </div>
+
+
+        </div>
+    </div>
+    <div class="trendingnowsection">
+        <div class="trendingnowsectionarticle">
+            <div class="trendingnowbanner"><p>Sale</p> </div>
+            <div class="trendingnowsectionarticleimg"><img src="img/trendingnowitem1img.png"/></div>
+            <div class="trendingnowsectionarticlecontent">
+                <h2>Shop t-shirt</h2>
+                <p>Lorem ipsum dolor sit amet, consectetuer</p>
+                <span class="trendingnowsectionarticlecontentpgray">$34,99</span>
+                <span class="trendingnowsectionarticlecontentpred">$24,99</span>
+                <div class="trendingnowcontentfooter">
+                <div class="trendingnowbuy"><i class="zmdi zmdi-shopping-cart-plus"></i></div><span>Add to cart</span>
+                </div>
+            </div>
+        </div>
+        <div class="trendingnowsectionarticle">
+            <div class="trendingnownotbanner"></div>
+            <div class="trendingnowsectionarticleimg"><img src="img/trendingnowitem2img.png"/></div>
+            <div class="trendingnowsectionarticlecontent">
+                <h2>Sneaker</h2>
+                <p>Lorem ipsum dolor sit amet, consectetuer</p>
+                <span class="trendingnowsectionarticlecontentpred">$24,99</span>
+                <div class="trendingnowcontentfooter">
+                    <div class="trendingnowbuy"><i class="zmdi zmdi-shopping-cart-plus"></i></div><span>Add to cart</span>
+                </div>
+            </div>
+        </div>
+        <div class="trendingnowsectionarticle">
+            <div class="trendingnownotbanner"></div>
+            <div class="trendingnowsectionarticleimg"><img src="img/trendingnowitem3img.png"/></div>
+            <div class="trendingnowsectionarticlecontent">
+                <h2>Backpack</h2>
+                <p>Lorem ipsum dolor sit amet, consectetuer</p>
+                <span class="trendingnowsectionarticlecontentpred">$24,99</span>
+                <div class="trendingnowcontentfooter">
+                    <div class="trendingnowbuy"><i class="zmdi zmdi-shopping-cart-plus"></i></div><span>Add to cart</span>
+                </div>
+            </div>
+        </div>
+        <div class="trendingnowsectionarticle">
+            <div class="trendingnowbanner"><p>Sale</p> </div>
+            <div class="trendingnowsectionarticleimg"><img src="img/trendingnowitem4img.png"/></div>
+            <div class="trendingnowsectionarticlecontent">
+                <h2>Heritage</h2>
+                <p>Lorem ipsum dolor sit amet, consectetuer</p>
+                <span class="trendingnowsectionarticlecontentpred">$24,99</span>
+                <div class="trendingnowcontentfooter">
+                    <div class="trendingnowbuy"><i class="zmdi zmdi-shopping-cart-plus"></i></div><span>Add to cart</span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="sponsors">
+        <div class="sponsorsbuttonleft"><i class="zmdi zmdi-chevron-left"></i></div>
+        <div class="sponsorslist">
+            <li><img src="img/sponsors1.png" alt="sponsors1"/></li>
+            <li><img src="img/sponsors2.png" alt="sponsors2"/></li>
+            <li><img src="img/sponsors3.png" alt="sponsors3"/></li>
+            <li><img src="img/sponsors1.png" alt="sponsors4"/></li>
+        </div>
+        <div class="sponsorsbuttonright"><i class="zmdi zmdi-chevron-right"></i></div>
+    </div>
+
 </div>
 
 </div>
